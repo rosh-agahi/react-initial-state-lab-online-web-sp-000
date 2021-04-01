8f5fcc8c8a9bf140272e2140ad1da10b4a181864
Roshanak Agahi <rosh.agahi@gmail.com>
2021-04-01 05:39:37 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,15 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+    constructor(props) {
+        super()
+        
+        }
+    }
+    render() {
+        return (
+
+        )
+    }
+}
